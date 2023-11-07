10210de096379a2cb55fd88f257e0d9c40740d74
Yaswanthypfm7 <98192925+Yaswanthypfm7@users.noreply.github.com>
2023-11-07 16:43:14 -0600

    changes to the UI
    Made changes to the sub heading,
    Icons and few more

--- src/myName/MyName.js
+++ src/myName/MyName.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "../styles/myName.css";
+import Typewriter from 'typewriter-effect';
+
 
 const MyName = () => {
   return (
     <div id="myName">
       <h1>YASWANTH PARUCHURI</h1>
+      {/* <div className="container">
+      <div className="typed-out">Fullstack Developer</div>
+      </div> */}
       <div className="container">
-      <div className="typed-out">Fullstack Developer</div></div>
+      <Typewriter
+        options={{
+        strings: ['Full Stack Developer', 'Front End Developer', 'Software Engineer'],
+        autoStart: true,
+        loop: true,
+        }
+      }/>
     </div>
+  </div>
   );
 };
 
